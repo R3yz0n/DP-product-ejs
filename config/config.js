0a5bdcbf1ec963fc90d50db3a0664466ad870c25
R3yz0n <reyzon.thapa2020@gmail.com>
2023-09-30 10:16:33 +0545

    production

--- config/config.js
+++ config/config.js
@@ -1,9 +1,37 @@
 // // For local environment
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "STORE_DP",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
+
+// module.exports = {
+//   HOST: "containers-us-west-123.railway.app",
+//   USER: "root",
+//   PASSWORD: "vnjkTxc6RgHmqLIHdTRn",
+//   DB: "railway",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
+
 module.exports = {
-  HOST: "localhost",
+  HOST: "containers-us-west-197.railway.app",
   USER: "root",
-  PASSWORD: "",
-  DB: "STORE_DP",
+  PASSWORD: "pZTFyoKza46gvP5jhS91",
+  DB: "railway",
   dialect: "mysql",
   pool: {
     max: 5,
@@ -12,18 +40,3 @@ module.exports = {
     idle: 10000,
   },
 };
-
-// for production
-// module.exports = {
-//     HOST: "containers-us-west-123.railway.app",
-//     USER: "root",
-//     PASSWORD: "vnjkTxc6RgHmqLIHdTRn",
-//     DB: "railway",
-//     dialect: "mysql",
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000,
-//     },
-//   };
--- model/index.js
+++ model/index.js
@@ -5,8 +5,8 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   operatorsAliases: false,
-  port: 3306,
-  // port: 6905,
+  // port: 3306,
+  port: 5733,
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
